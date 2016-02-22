4a0a9b55c528ccb7ba681012499b2d7694e45dcf
Ryan Krolick <rkrolick@gmail.com>
2016-02-22 12:42:53 -0500

    cards now display value when flipped

--- script.js
+++ script.js
@@ -1,34 +1,48 @@
 var cardArea = {
   cards: [],
+  possibleValues: ["wood", "metal", "water"],
 
   createCardArea:function(numCards){
     if ((numCards % 2) != 0){numCards+=1;}
     var area = document.getElementsByClassName("cardArea")[0];
     for (var i = 0; i < numCards; i++){
+
+      //Create DOM objects.
       div = document.createElement('div');
       div.className = "card";
       area.appendChild(div);
       div.setAttribute("id", i);
       div.addEventListener("click", flip);
+
+      // Create cardArea Objects.
+
       var card = {
-        flipStat: false,
+        bFlipped: false,
         value: i
       }
       this.cards.push(card);
     }
+
+    this.shuffleCards();
+  },
+
+  shuffleCards: function(){
+
   }
 }
 
 function flip(event){
-  var cardNum = Number(this.id);;
-  if(!cardArea.cards[cardNum].flipStat){
+  var cardNum = Number(this.id);
+  if(!cardArea.cards[cardNum].bFlipped){
     this.style.backgroundColor="rgb(255, 0, 0)";
-    cardArea.cards[cardNum].flipStat = true;
+    cardArea.cards[cardNum].bFlipped = true;
+    this.appendChild(document.createTextNode(cardArea.cards[cardNum].value));
   }
   else {
     this.style.backgroundColor="rgb(0, 0, 255)";
     cardNum = Number(this.id);
-    cardArea.cards[cardNum].flipStat = false;
+    cardArea.cards[cardNum].bFlipped = false;
+    this.removeChild(this.childNodes[0]);
   }
 }
 
