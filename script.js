b3e2602d0195cd25bd1413db5869669a5beec1d5
Ryan Krolick <rkrolick@gmail.com>
2016-02-22 12:14:25 -0500

    added ability to keep track of individual card objects

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
+var cardArea = {
+  cards: [],
 
-
-function createCardArea(numCards){
-  var area = document.getElementsByClassName("cardArea")[0];
-  var card;
-  for (var i = 0; i < numCards; i++){
-    div = document.createElement('div');
-    div.className = "card";
-    area.appendChild(div);
+  createCardArea:function(numCards){
+    if ((numCards % 2) != 0){numCards+=1;}
+    var area = document.getElementsByClassName("cardArea")[0];
+    for (var i = 0; i < numCards; i++){
+      div = document.createElement('div');
+      div.className = "card";
+      area.appendChild(div);
+      div.setAttribute("id", i);
+      div.addEventListener("click", flip);
+      var card = {
+        flipStat: false,
+        value: i
+      }
+      this.cards.push(card);
+    }
   }
 }
 
-createCardArea(22);
+function flip(event){
+  var cardNum = Number(this.id);;
+  if(!cardArea.cards[cardNum].flipStat){
+    this.style.backgroundColor="rgb(255, 0, 0)";
+    cardArea.cards[cardNum].flipStat = true;
+  }
+  else {
+    this.style.backgroundColor="rgb(0, 0, 255)";
+    cardNum = Number(this.id);
+    cardArea.cards[cardNum].flipStat = false;
+  }
+}
+
+cardArea.createCardArea(32);
