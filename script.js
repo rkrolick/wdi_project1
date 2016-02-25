7ede42dbce01deff0924f03d995f599c8840446c
Ryan Krolick <rkrolick@gmail.com>
2016-02-24 22:42:49 -0500

    added basic collision, now need to be near cards to select

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 //////////////////////////////////////////
 // Declare constants used by game logic.//
 //////////////////////////////////////////
+
+// Card Display constants
 var CARD_FACE_COLOR = "rgb(255, 255, 0)";
 var CARD_BACK_COLOR = "rgb(0, 0, 255)";
 var CARD_INACTIVE_COLOR = "rgb(125, 125, 125)";
 var CORRECT_FLASH_COLOR = "rgb(0, 255, 0)";
 var INCORRECT_FLASH_COLOR = "rgb(255, 0, 0)";
+
+// Player constants
+var P_START_X = 0;
+var P_START_Y = 0;
+var P_OFFSET_X = 30; // TODO: Find a way to calculate this dynamically based on view port.
+var P_OFFSET_Y = 8; // TODO: Find a way to calculate this dynamically based on view port.
+var P_SIZE = 60;
+var P_SPEED = 6;
+
+// CollisionGrid constants
+var GRID_LENGTH = 31;
+var GRID_HEIGHT = 13;
+var NODE_SIZE = 60;
+
+// GameCards constants
+var CARD_AMOUNT = 30;
 
 //////////////////////////////////////////////////
 // Declare global variables used by game logic. //
@@ -20,10 +38,126 @@ var KILL = false; // debug purposes only.  If set true via the console, this wil
 
 // Keeps track of player data.
 var player = {
-  x: 8,
-  y: 8,
-  height: 30,
-  width: 30
+  x: P_START_X,
+  y: P_START_Y,
+  xOffset: P_OFFSET_X,  // Used by display functon to correctly position player based the offset of cardArea.
+  yOffset: P_OFFSET_Y, // Used by display functon to correctly position player based the offset of cardArea.
+  //gridX: null,
+  //gridY: null,
+  onNode: null,
+  //transitionNode: null, // When moving between Nodes  account for both.
+  bInLineWithGrid: true,
+  size: P_SIZE,
+  speed: P_SPEED,
+
+  // Initialize player
+  init: function(){
+    this.updateGridPos();
+  },
+
+  // Updates player position data based on input recieved.
+  move: function(direction){
+    if(this.checkCollision(direction)){return;}
+    switch (direction){
+      case ("up"): this.y -= this.speed; break;
+      case ("down"): this.y += this.speed; break;
+      case ("left"): this.x -= this.speed; break;
+      case ("right"): this.x += this.speed; break;
+    }
+
+    this.updateGridPos();
+  },
+
+  updateGridPos: function(){
+    this.onNode = collisionGrid.getNode(this.x , this.y);
+  },
+
+  checkCollision: function(direction){
+    var corner1;
+    var corner2;
+
+    switch (direction){
+      case ("up"):
+        corner1 = collisionGrid.valueAt(collisionGrid.getNode(this.x, this.y-1));
+        corner2= collisionGrid.valueAt(collisionGrid.getNode(this.x+this.size-1, this.y-1));
+        if ((corner1 == null && corner2 == null)){return false;} else {return true;}
+
+      case ("down"):
+        corner1 = collisionGrid.valueAt(collisionGrid.getNode(this.x, this.y+this.size));
+        corner2 = collisionGrid.valueAt(collisionGrid.getNode(this.x+this.size-1, this.y+this.size));
+        if ((corner1 == null && corner2 == null)){return false;} else {return true;}
+
+      case ("left"):
+        corner1 = collisionGrid.valueAt(collisionGrid.getNode(this.x-1, this.y));
+        corner2 = collisionGrid.valueAt(collisionGrid.getNode(this.x+this.size, this.y));
+        if ((corner1 == null && corner2 == null)){return false;} else {return true;}
+
+      case ("right"):
+        corner1 = collisionGrid.valueAt(collisionGrid.getNode(this.x+this.size, this.y));
+        corner2 = collisionGrid.valueAt(collisionGrid.getNode(this.x+this.size, this.y+this.size-1));
+        if ((corner1 == null && corner2 == null)){return false;} else {return true;}
+      default: return true;
+    }
+  },
+
+  adjacentCards: function(){
+    var adjCards = [];
+
+    adjCards.push(collisionGrid.valueAt(this.onNode - collisionGrid.length)); // above
+    adjCards.push(collisionGrid.valueAt(this.onNode + collisionGrid.length)); // below
+    adjCards.push(collisionGrid.valueAt(this.onNode + 1)); // right TODO: technically doesnt give correct results
+    adjCards.push(collisionGrid.valueAt(this.onNode - 1)); // left TODO: technically doesnt give correct results
+
+    return adjCards;
+
+
+  }
+}
+
+// Organizes the game space into nodes to check for object interactions/collisions
+var collisionGrid = {
+  grid: [],
+  length: GRID_LENGTH,
+  height: GRID_HEIGHT,
+  nodeSize: NODE_SIZE,
+
+  // Builds an array that abstracts the game space.  An individual element is null if
+  // it is not solid.  Else it contains a number representing a card.
+  buildGrid: function(){
+    var row = [];
+    var cardNumStartThisRow = -10;
+    var cardNumStartThisCol = -1;
+
+    for (var i = 0; i < this.height; i++){
+      for (var j = 0; j < this.length; j++){
+        if(i%4 == 0){row.push(null);}
+        else if(j%3 == 0){row.push(null); cardNumStartThisCol++}
+        else row.push(cardNumStartThisRow + cardNumStartThisCol);
+      }
+      this.grid.push(row);
+      if(i%4 == 0){cardNumStartThisRow += 10;}
+      cardNumStartThisCol = -1;
+      row = [];
+    }
+  },
+
+  // returns node number at pixel coordinate x
+  getNode: function (x, y){
+    var nodeRow = Math.floor((y / this.nodeSize));
+    var nodeCol = Math.floor((x / this.nodeSize));
+    return (nodeRow * this.length + nodeCol);
+  },
+
+  // Returns the value of a node specified by nodeNumber
+  valueAt: function(nodeNumber){
+    if ((nodeNumber >= 0) && (nodeNumber <= (this.length * this.height))){
+      var y = nodeNumber%this.length;
+      var x = (nodeNumber - y) /this.length;
+      return this.grid[x][y];
+    }
+    return 666;
+  }
+
 }
 
 // keeps track of cards that are flashing.
@@ -48,7 +182,7 @@ var gameCards = {
   previousGuess: null, // Keeps tabs on the 1st guess of a pair, via a number that corresponds to an index of the cards array.  If null, a guess has not been made.
   cardTypes: ["e","p","a","b","f","w"], // Defines possible card types.  Elevator needs to be first in the array and powercell second for correct game logic.
   typeAmounts: [ 1, 5, 2, 10, 10, 2], // Each index corresponds with index in cardTypes array, specifying amount to distribute.  Items need to total totalCards for correct game logic.
-  totalCards: 30, // Defines how many cards are displayed.
+  totalCards: CARD_AMOUNT, // Defines how many cards are displayed.
   bFlashing: false, // Flag that forces selectCard to ignore user input.  Used if an animation needs to be updated before user input can be processed.
 
   // Builds deck based on the types and distrubution defined by the cardTypes & typeAmounts arrays.
@@ -71,6 +205,7 @@ var gameCards = {
 
   // Selects the cardSpecified by the user, updates previousGuess accordingly, checks for matches if appropriate.
   selectCard: function(i){
+    if (!player.adjacentCards().includes(i)){return;} // if player is not next to clicked card, ignore.
     if (i == this.previousGuess){return;} // Do nothing if selected card was chosen on prior selection.
     if (!this.deck[i].bActive){return;} // Do nothing if selected card is inactive.
     if (this.bFlashing) {return;}  // Do nothing if told to hold (due to flashing animation)
@@ -165,10 +300,10 @@ function flip(event){
 // Handles keyboard event on player.
 function movePlayer(event){
   switch (event.keyCode){
-    case (87): player.y -= 3; break;
-    case (83): player.y += 3; break;
-    case (65): player.x -= 3; break;
-    case (68): player.x += 3; break;
+    case (87): player.move("up"); break;
+    case (83): player.move("down"); break;
+    case (65): player.move("left"); break;
+    case (68): player.move("right"); break;
   }
 }
 
@@ -227,8 +362,8 @@ function display(){
     }
   }
   //Ends display of cards
-  document.getElementsByClassName("player")[0].style.top = player.y + "px";
-  document.getElementsByClassName("player")[0].style.left = player.x + "px";
+  document.getElementsByClassName("player")[0].style.top = player.y + player.yOffset + "px";
+  document.getElementsByClassName("player")[0].style.left = player.x + player.xOffset + "px";
 }
 
 
@@ -247,5 +382,7 @@ function gameLoop(){
 ///////////////////////////
 window.addEventListener("keydown", movePlayer);
 gameCards.buildDeck(true);
+collisionGrid.buildGrid();
+player.init();
 buildDOM();
 setInterval(gameLoop, 30);
